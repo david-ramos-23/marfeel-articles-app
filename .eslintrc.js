f3054e241a25c32360c371f0df58b5d9ac76e729
David Ramos <andara14@gmail.com>
2020-06-08 13:44:06 +0200

    add: eslint, prettier, husky, lint-staged, styled-components & enzyme

---
+++ .eslintrc.js
@@ -0,0 +1,15 @@
+// .eslintrc.js
+module.exports = {
+  root: true,
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint', 'prettier'],
+  rules: {
+    'prettier/prettier': 'error',
+  },
+  extends: [
+    'react-app',
+    'eslint:recommended',
+    'plugin:@typescript-eslint/eslint-recommended',
+    'plugin:@typescript-eslint/recommended',
+  ],
+};
