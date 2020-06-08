0015e422ea339332ab55ce7695a92c54efa8c067
David Ramos <andara14@gmail.com>
2020-06-08 17:06:32 +0200

    add: Main with MediaBlock from react-placeholders

--- src/components/Main/Main.tsx
+++ src/components/Main/Main.tsx
@@ -1,3 +1,36 @@
 import React from 'react';
+import { ThemeConsumer } from 'styled-components';
+import { StyledMain, StyledMainMediaBlock } from './styled';
 
-export const Main: React.FunctionComponent = () => <section>Main</section>;
+export const Main: React.FunctionComponent = () => (
+  <ThemeConsumer>
+    {(theme) => (
+      <StyledMain>
+        <StyledMainMediaBlock color={theme.colors.gray1} rows={4} />
+        <StyledMainMediaBlock color={theme.colors.gray1} rows={4} />
+        <StyledMainMediaBlock color={theme.colors.gray1} rows={4} />
+        <StyledMainMediaBlock color={theme.colors.gray1} rows={4} />
+        <StyledMainMediaBlock color={theme.colors.gray1} rows={4} />
+        <StyledMainMediaBlock color={theme.colors.gray1} rows={4} />
+        <StyledMainMediaBlock color={theme.colors.gray1} rows={4} />
+        <StyledMainMediaBlock color={theme.colors.gray1} rows={4} />
+        <StyledMainMediaBlock color={theme.colors.gray1} rows={4} />
+        <StyledMainMediaBlock color={theme.colors.gray1} rows={4} />
+        <StyledMainMediaBlock color={theme.colors.gray1} rows={4} />
+        <StyledMainMediaBlock color={theme.colors.gray1} rows={4} />
+        <StyledMainMediaBlock color={theme.colors.gray1} rows={4} />
+        <StyledMainMediaBlock color={theme.colors.gray1} rows={4} />
+        <StyledMainMediaBlock color={theme.colors.gray1} rows={4} />
+        <StyledMainMediaBlock color={theme.colors.gray1} rows={4} />
+        <StyledMainMediaBlock color={theme.colors.gray1} rows={4} />
+        <StyledMainMediaBlock color={theme.colors.gray1} rows={4} />
+        <StyledMainMediaBlock color={theme.colors.gray1} rows={4} />
+        <StyledMainMediaBlock color={theme.colors.gray1} rows={4} />
+        <StyledMainMediaBlock color={theme.colors.gray1} rows={4} />
+        <StyledMainMediaBlock color={theme.colors.gray1} rows={4} />
+        <StyledMainMediaBlock color={theme.colors.gray1} rows={4} />
+        <StyledMainMediaBlock color={theme.colors.gray1} rows={4} />
+      </StyledMain>
+    )}
+  </ThemeConsumer>
+);
---
+++ src/components/Main/styled.ts
@@ -0,0 +1,17 @@
+import styled from 'styled-components';
+
+// import MediaBlock from 'react-placeholder/lib/placeholders/MediaBlock';
+import { MediaBlock } from 'react-placeholder/lib/placeholders';
+
+export const StyledMainMediaBlock = styled(MediaBlock)`
+  padding: ${(props) => props.theme.sizes.size1};
+  margin-top: ${(props) => props.theme.sizes.size1}
+  border: 1px solid ${(props) => props.color};
+  :first-child {
+    margin-top: 0;
+  }
+`;
+
+export const StyledMain = styled.section`
+  padding: ${(props) => props.theme.sizes.size1};
+`;
